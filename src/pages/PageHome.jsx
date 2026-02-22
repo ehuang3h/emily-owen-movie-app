70eb047ddb28e3020c97cf81efa069a29d0da31a
Owen Chan <git@owenc.ca>
2026-02-21 23:41:01 -0800

    filter underline sliding effect added

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -93,7 +93,7 @@ function PageHome(){
 			<section>
 				<div className='search-filter-bar'>
 					{/* filter btns */}
-					<div className="movie-filters">
+					<div className="movie-filters home-filter-grid">
 						
 						<button className={activeLink === 'Now Playing' ? 'active' : ''}onClick={() => {setFilter(nowPlaying); setActiveLink('Now Playing');}}>Now Playing</button>
 						<button className={activeLink === 'Top Rated' ? 'active' : ''}onClick={() => {setFilter(topRated); setActiveLink('Top Rated');}}>Top Rated</button>
